7448e1c4e9dddcf24d2a738abbffd6c84619bddf
Zac Merritt <zmerdev@gmail.com>
2022-10-28 12:47:08 -0400

    idk

--- src/App.js
+++ src/App.js
@@ -13,42 +13,14 @@ import {
 
 export default function App() {
   return (
-    <Router basename='/SimCoRetailOptimizer'>
-
-      <div className='main-wrapper main-wrapper-responsive-lg'>
-        <SidebarMenu bg="dark" collapseOnSelect={false} defaultExpanded={true} exclusiveExpand={false} expand="lg" hide="md" variant="dark">
-          <SidebarMenu.Collapse>
-            <SidebarMenu.Header>
-              <SidebarMenu.Brand title="React-Bootstrap">
-                <span className="react-bootstrap-img" />
-              </SidebarMenu.Brand>
-              <SidebarMenu.Toggle />
-            </SidebarMenu.Header>
-            <SidebarMenu.Body>
-              <SidebarMenu.Nav>
-                <SidebarMenu.Nav.Link href='/SimCoRetailOptimizer/retail' >
-                  <SidebarMenu.Nav.Title>
-                    {"Retail"}
-                  </SidebarMenu.Nav.Title>
-                </SidebarMenu.Nav.Link>
-              </SidebarMenu.Nav>
-              <SidebarMenu.Nav>
-                <SidebarMenu.Nav.Link href='/SimCoRetailOptimizer/building-production'>
-                  <SidebarMenu.Nav.Title>
-                    {"Buildings"}
-                  </SidebarMenu.Nav.Title>
-                </SidebarMenu.Nav.Link>
-              </SidebarMenu.Nav>
-            </SidebarMenu.Body>
-          </SidebarMenu.Collapse>
-        </SidebarMenu>
-
+    <div className='main-wrapper main-wrapper-responsive-lg'>
+      <Router>
         <Routes>
           <Route path="/retail" element={<Retail />} />
           <Route path="/buildings" element={<ProductionBuildingsNeeded />} />
-          <Route path="/" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
         </Routes>
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
