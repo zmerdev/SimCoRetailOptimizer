a378d1ff895e8ab3222ff17644adc1cb582cee2f
Zac Merritt <zmerdev@gmail.com>
2022-10-28 12:58:47 -0400

    add back sidebar

--- src/App.js
+++ src/App.js
@@ -9,11 +9,38 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import SidebarMenu from 'react-bootstrap-sidebar-menu';
 
 export default function App() {
   return (
     <div className='main-wrapper main-wrapper-responsive-lg'>
       <Router>
+        <SidebarMenu bg="dark" collapseOnSelect={false} defaultExpanded={true} exclusiveExpand={false} expand="lg" hide="md" variant="dark">
+          <SidebarMenu.Collapse>
+            <SidebarMenu.Header>
+              <SidebarMenu.Brand title="React-Bootstrap">
+                <span className="react-bootstrap-img" />
+              </SidebarMenu.Brand>
+              <SidebarMenu.Toggle />
+            </SidebarMenu.Header>
+            <SidebarMenu.Body>
+              <SidebarMenu.Nav>
+                <SidebarMenu.Nav.Link href='/#/retail' >
+                  <SidebarMenu.Nav.Title>
+                    {"Retail"}
+                  </SidebarMenu.Nav.Title>
+                </SidebarMenu.Nav.Link>
+              </SidebarMenu.Nav>
+              <SidebarMenu.Nav>
+                <SidebarMenu.Nav.Link href='/#/building-production'>
+                  <SidebarMenu.Nav.Title>
+                    {"Buildings"}
+                  </SidebarMenu.Nav.Title>
+                </SidebarMenu.Nav.Link>
+              </SidebarMenu.Nav>
+            </SidebarMenu.Body>
+          </SidebarMenu.Collapse>
+        </SidebarMenu>
         <Routes>
           <Route path="/retail" element={<Retail />} />
           <Route path="/buildings" element={<ProductionBuildingsNeeded />} />
